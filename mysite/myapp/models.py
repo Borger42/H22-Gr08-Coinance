c95b385c79e41cf92b737fbe5666c919d536ce88
RedSquirrels <98779708+RedSquirrels@users.noreply.github.com>
2022-04-25 15:07:16 -0400

    admin creer des utilisateurs

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    nom = models.CharField(max_length=200, null=True)
+    
+    name = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
-    nom = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=200, null=True)
     prix = models.FloatField(null=True)
     date = models.DateTimeField(auto_now_add=True, null=True)
 
