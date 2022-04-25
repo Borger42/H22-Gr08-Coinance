e4702f23725d5ad503758437ee1413d43763e5f9
eliohajjar <hajjar2elio@gmail.com>
2022-04-25 10:42:55 -0400

    Update Url pour afficher la page home comme page initiale

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,8 +19,9 @@ from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
-    path('', include('myapp.urls')),
+    path('test', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
     path('home/', views.home, name="home"),
     path('aboutus/', views.aboutus, name="aboutus"),
     path('actions/<str:action>', views.Actions, name="actions"),
